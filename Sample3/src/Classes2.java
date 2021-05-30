64015214f4aac08f6a5a491ca03be45a684198ce
Dineshkm23 <dineshdk.tdkm@gmail.com>
2021-05-30 08:43:29 +0530

    classes2 added and message 3 added in classws1 added Message 4

---
+++ Sample3/src/Classes2.java
@@ -0,0 +1,10 @@
+
+public class Classes2 {
+
+	public static void main(String[] args)
+	{
+	System.out.println("Message3 added in Classes2");
+
+	}
+
+}
--- Sample3/src/Classws1.java
+++ Sample3/src/Classws1.java
@@ -6,6 +6,7 @@ public class Classws1
 	{
 		// TODO Auto-generated method stub
 		System.out.println("message 2 in classws1");
+		System.out.println("Message4 added in classws1");
 	}
 
 }
